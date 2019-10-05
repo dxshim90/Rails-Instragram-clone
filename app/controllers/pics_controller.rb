c138ce8fbe606874dc72dc395471389bd39952a2
dxshim90 <dxshim90@gmail.com>
2019-10-05 17:02:03 +0100

    Created edit, update and destroy methods and veiw files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,7 +1,8 @@
 class PicsController < ApplicationController
+    before_action :find_pic, only: [:edit, :update, :show, :destroy]
 
     def index 
-
+        @pics = Pic.all.order('created_at DESC')
     end
 
     def new 
@@ -11,16 +12,40 @@ class PicsController < ApplicationController
 
     def create 
         @pic = Pic.new(pic_params)
+        if @pic.save()
+            redirect_to @pic, notice: "Pic was created"
+        else
+            render 'new'
+        end
+    end
+
+    def show 
+    end
+
+    def edit 
+    end
+
+    def update 
+        if @pic.update(pic_params)
+            redirect_to @pic, notice: "Pic Has been Updated"
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy 
+        @pic.destroy
+        redirect_to root_path
     end
 
     private 
 
     def find_pic
-
+        @pic = Pic.find(params[:id])
     end
 
     def pic_params 
-        params.require(pic).permit(:title, :description)
+        params.require(:pic).permit(:title, :description)
     end
 
 end
